c03bdb485be3f735ad306f4f7d7f3cbbffe7ffb5
clam c <clam0540@gmail.com>
2023-01-11 15:21:40 -0700

    the

--- port.js
+++ port.js
@@ -1,5 +1,8 @@
 const number = Math.floor((Math.random()) * 6)
+const number2= Math.floor((Math.random()) * 6)
 
-const ifs = ['Amber','Isles','Maeve','Nevada','Sloane','Trixie']
+const ifs = ['Amber','Isla','Mavis','Addy','Sloane','Trixie']
+const ifs2 = ['Izzy','Dallas','Luna','Tirza','Melanie','Ari']
 
 console.log(ifs[number])
+console.log(ifs2[number2])
